efeb37fe99df2fd3550285752676845e25f9eaa4
Mike van Rossum <mike@askmike.org>
2012-01-04 18:16:47 +0100

    added checks to see if there is a navigation

--- views/blog.php
+++ views/blog.php
@@ -2,8 +2,7 @@
 		<h1>
 			Blog	
 		</h1>
-		<div id='blog-posts'>
-			
+		<div id='blog-posts'>			
 				<?php foreach ($data as $item) {
 				//we only want arays in the array data
 				if(is_array($item)) { ?> 
@@ -23,7 +22,12 @@
 				<?php } } ?>
 				
 		<div id="blog-nav">
+			<?php //lastpage does not exist yet, so right now it always passes this if
+			if(!$lastpage) { ?>
 			<a href='#'>&#60; oudere posts</a>
+			<?php } ?>
+			<?php if(!$firstpage) { ?>
 			<a href='#'>nieuwere posts &#62;</a>
+			<?php } ?>
 		</div>
 	</section>
