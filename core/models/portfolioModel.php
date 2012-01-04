fd7471ff31d7111a49667539eafb8005284af652
Mike van Rossum <mike@askmike.org>
2012-01-04 03:12:53 +0100

    cleaned up model

--- core/models/portfolioModel.php
+++ core/models/portfolioModel.php
@@ -7,15 +7,10 @@ class PortfolioModel extends DBmodel {
 	
 	
 	public function getPortfolio() {
-		//get events from DB
+		
 		$members = $this->connection->query('SELECT * FROM portfolio ORDER BY id DESC');
 		
-		//store events in an array
-		while ($row = $members->fetch_assoc()) {
-			$rows[] = $row;
-		}
-		//return the array
-		return $rows;
+		return $this->assocResults($members);
 	}
 	
 	
