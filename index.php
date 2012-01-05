5da1e76391ed5366377c10d9a786e599e8633c56
Mike van Rossum <mike@askmike.org>
2012-01-05 14:12:25 +0100

    preparing for different parses

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 //so I can time how long the it takes before we sent it back to the client
 //when we get to views/footer.php we compare this with then.
-define('START', microtime(true));
+define('STARTTIME', microtime(true));
 
 //define basic things we need before we can require anything at all
 define('APP', 'core/');
@@ -16,21 +16,18 @@ require APP . 'config.php';
 $request = explode('/',$_GET['r']);
 
 //route
-if (true) { 
-	require CONTROLLERS . 'siteController.php';
-	$con = new SiteController($request);
+if ($request[0] == 'json') { 
+	//it's a json request
+	//return json and die asap
 
-} else if ($request[0] == 'leden' || $request[0] == 'lid') { 
-	// leden or member
-	
-//	require CONTROLLERS . 'memberController.php';
-//	$con = new MemberController($request);
+} else if ($request[0] == 'leden') { 
+	//empty
 	
 } else { 
 	//home or 404
 	
-//	require CONTROLLERS . 'homeController.php';
-//	$con = new HomeController($request);
+	require CONTROLLERS . 'siteController.php';
+	$con = new SiteController($request);
 	
 } 
 
