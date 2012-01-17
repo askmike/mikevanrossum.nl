1e6923090d29943ccb464f6f8d406da14cf5b5ce
Mike van Rossum <mike@askmike.org>
2012-01-17 16:16:26 +0100

    routing moved to it's own class
    So in the future we could re-route after init

---
+++ core/route.php
@@ -0,0 +1,70 @@
+<?php
+
+class Route {
+	
+	function __construct($request) {
+		
+		//parse request
+		if(is_string($request)) $request = explode('/',$request);
+
+		//route
+		switch ($request[0]) {
+		    case 'blog':
+		        //single post request
+
+				$con = new PostController($_GET['r']);
+
+		        break;
+			case 'analytics':
+		        //single post request
+
+				$con = new AnalyticsController;
+				$con->postStatistics($request);
+
+		        break;	
+		    case 'track':
+				// it's a track request: some analytics data getting passed
+				// we don't return anything
+
+				$con = new TrackController;
+
+		        break;
+		    case 'admin':
+				// backend time
+
+				$con = new AdminController($request, $_GET['r']);
+
+		        break;
+			case 'json':
+				// it's a json request
+
+				$con = new PostsController($request[2]);
+
+				$con->jsonData();
+
+		        break;
+			case '':
+				// home
+
+				$con = new SiteController($request);
+
+		        break;
+
+			case 'sitemap.xml':
+				// it's a sitemap request
+
+				$con = new SitemapController;
+
+		        break;
+
+
+			default:
+				// 404
+
+				new ErrorController;
+		}
+		
+	}
+}
+
+?>
--- index.php
+++ index.php
@@ -13,60 +13,8 @@ define('MODELS', APP . 'models/');
 require APP . 'config.php';
 require APP . 'lazyloading.php';
 
-//parse request
-$request = explode('/',$_GET['r']);
-
-//route
-switch ($request[0]) {
-    case 'blog':
-        //single post request
-
-		$con = new PostController($_GET['r']);
-		
-        break;
-    case 'track':
-		// it's a track request: some analytics data getting passed
-		// we don't return anything
-
-		$con = new TrackController;
-
-        break;
-    case 'admin':
-		// backend time
-
-		$con = new AdminController($request, $_GET['r']);
-
-        break;
-	case 'json':
-		// it's a json request
-		
-		$con = new PostsController($request[2]);
-		
-		$con->jsonData();
-		
-        break;
-	case '':
-		// home
-
-		$con = new SiteController($request);
-
-        break;
-
-	case 'sitemap.xml':
-		// it's a sitemap request
-
-		$con = new SitemapController;
-
-        break;
-
-
-	default:
-		// 404
-		
-		new ErrorController;
-}
-
-
+//route parses the request and serves accordingly
+new Route($_GET['r']);
 
 //end with killing the connection
 if($con) $con = NULL;
