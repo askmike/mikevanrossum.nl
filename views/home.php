9a3a8a8c721d08f0d7a86784ae6fbfcd9acd1d3f
Mike van Rossum <mike@askmike.org>
2012-01-18 12:07:39 +0100

    updated site to the live version on mikevanrossum.nl/

--- views/home.php
+++ views/home.php
@@ -3,21 +3,13 @@
 				home	
 			</h1>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec risus 
-				dolor, eget egestas ipsum. Ut tellus libero, scelerisque a porta sceler
-				isque, gravida sed mi. Fusce mollis aliquet mauris id condimentum. Done
-				c ullamcorper, nunc in accumsan mattis, turpis libero tincidunt ligula, 
-				non convallis mauris felis in tellus. Quisque commodo turpis non ante 
-				sollicitudin mattis. Maecenas placerat felis eget lectus pharetra cons
-				equat. Vivamus porttitor sollicitudin elit at feugiat. Quisque nec met
-				us sed est tincidunt egestas id id libero. Fusce mattis elementum tell
-				us sit amet dignissim. Vestibulum ante nisi, interdum in dictum ut, ma
-				ttis quis orci. Sed justo nunc, imperdiet congue mattis nec, eleifend
-				 eget tellus. Maecenas ultricies iaculis luctus. Duis fermentum vesti
-				bulum nulla in viverra.
+				Currently under construction, stay tuned!
 			</p>
-			<div class='a'>a</div>
-			<div class='b'>b</div>
+			<p>	
+				Check out my <a href='#blog'>blog</a> or <a href='#portfolio'>portfolio</a>, both are up and running!</a>
+			</p>
+			<!-- 			<div class=a>a</div>
+						<div class=b>b</div> -->
 		</section>
 		<section class='page'>
 			<h1>
