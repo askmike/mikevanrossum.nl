6b25593475d768b33f2d4620326f950500cea79d
Mike van Rossum <mike@askmike.org>
2012-01-20 18:12:18 +0100

    dirname is now stored in a var

--- cron/job.php
+++ cron/job.php
@@ -2,20 +2,22 @@
 
 define('CRON',true);
 
+$path = dirname(__FILE__);
+
 // this prefix makes it possible to require while cron'ing this
 // because the cron session has it's own working dir (screws up relative paths)
-require dirname(__FILE__) . '/../core/config.php';
+require $path . '/../core/config.php';
 
 // this is not working somehow, I'll do it manually for now
 // require dirname(__FILE__) . '/../core/lazyloading.php';
 
-require dirname(__FILE__) . '/../core/models/dBmodel.php';
+require $path . '/../core/models/dBmodel.php';
 
 //tweet
-require dirname(__FILE__) . '/../core/models/tweetModel.php';
-require dirname(__FILE__) . '/fetchTweets.php';
+require $path . '/../core/models/tweetModel.php';
+require $path . '/fetchTweets.php';
 //mijnrealiteit
-require dirname(__FILE__) . '/../core/models/mijnrealiteitModel.php';
-require dirname(__FILE__) . '/fetchMijnrealiteit.php';
+require $path . '/../core/models/mijnrealiteitModel.php';
+require $path . '/fetchMijnrealiteit.php';
 
 ?>
