5a1e7f2253d24d7c79e2839b9659d5b6ce1b90a1
Mike van Rossum <mike@askmike.org>
2012-01-23 01:01:14 +0100

    fixed RSS encoding problems

--- core/controllers/protocolController.php
+++ core/controllers/protocolController.php
@@ -27,22 +27,40 @@ class ProtocolController extends Controller {
 		
 		// I provide dates, those need to be in english
 		$data = $this->addEnglishDates($data);
+		$data = $this->encodeItems($data);
+		
+		header('Content-Type: application/rss+xml; charset=ISO-8859-1');
 		
 		$this->load->view('protocol/feed',$data);
 	}
 	
 	function addEnglishDates($array) {
-		if(is_array($array)) {
-			//we can't use a for each loop because we need to manip the array we're looping
-			$len = sizeof($array);
-			for($i = 0; $i < $len ; $i++){
-				if(is_array($array[$i])) {
-					$array[$i]['edate'] = date('D, j F Y',$array[$i]['date']);
-				}
+		$len = sizeof($array);
+		for($i = 0; $i < $len ; $i++){
+			if(is_array($array[$i])) {
+				$array[$i]['edate'] = date('D, j M Y h:i:s',$array[$i]['date']) . ' +0100';
 			}
 		}
 		return $array;
 	}
+	
+	function encodeItems($array) {
+		$len = sizeof($array);
+		for($i = 0; $i < $len ; $i++){
+			if(is_array($array[$i])) {
+				$array[$i]['excerpt'] = $this->encodeString($array[$i]['excerpt']);
+				$array[$i]['titel'] = $this->encodeString($array[$i]['titel']);
+				$array[$i]['edate'] = $this->encodeString($array[$i]['edate']);
+			}
+		}
+		return $array;
+	}
+	
+	function encodeString($str) {
+		return mb_convert_encoding($str, "ISO-8859-1", "UTF-8");
+	}
+	
+	
 	
 	function __destruct() {
 		//this runs the destruct of the class this class is extending
--- views/protocol/feed.php
+++ views/protocol/feed.php
@@ -1,22 +1,21 @@
 <?= '<?' 
 // this prevents PHP from trying to parse the first line 
 ?>xml version="1.0" encoding="ISO-8859-1"?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 	<channel>
 	        <title>Mike van Rossum</title>
+			<atom:link href="<?= SITE ?>feed.rss" rel="self" type="application/rss+xml"/>
 	        <link>http://www.mikevanrossum.nl/</link>
 	        <description>I blog about webdevelopment</description>
-	        <item>
-	            <?php foreach ($data as $row) { ?>
-				<item>
-					<title><?= $row['titel'] ?></title>
-					<link><?= SITE . $row['url'] ?></link>
-					<description><?= $row['excerpt'] ?></description>
-					<author>Mike van Rossum</author>
-					<pubDate><?= $row['edate'] ?></pubDate>
-				</item>
-				<?php } ?>
-	        </item>
+            <?php foreach ($data as $row) { ?>
+			<item>
+				<title><?= $row['titel'] ?></title>
+				<link><?= SITE . $row['url'] ?></link>
+				<description><?= $row['excerpt'] ?></description>
+				<pubDate><?= $row['edate'] ?></pubDate>
+				<guid isPermaLink="true"><?= SITE . $row['url'] ?></guid>
+			</item>
+			<?php } ?>
 	</channel>
 </rss>
 <!--# <?= 'feed served in: ' . round( (microtime( true ) - STARTTIME), 5 ) . ' seconds' ?> -->
